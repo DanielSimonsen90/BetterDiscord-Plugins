83729fc46e39a247acab490bfff4b72d49c94ede
DanielSimonsen90 <danielsimonsen90@gmail.com>
2024-10-10 00:34:39 +0200

    Delete archived plugins

--- archived/danho/DanhoBetterChat/Commands/spag-smells.tsx
+++
@@ -1,9 +0,0 @@
-import { buildCommand } from './CommandsModule';
-
-type Props = {}
-
-export default buildCommand<Props>("spag-smells", "Tell spag he smells", '', ["smelly"], (Lib, React, Message, props) => {
-    const { UserStore } = Lib.Libraries.ZLibrary.DiscordModules;
-    const spag = Object.values(UserStore.getUsers()).find(user => user.username === "Pebbles");
-    return <Message content={`@${spag.username} you smell lol`} suppressPing />
-})
