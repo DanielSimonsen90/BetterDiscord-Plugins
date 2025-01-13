2a1ed79d67c627b114a6cffc090a47b34e7e26ae
DanielSimonsen90 <danielsimonsen90@gmail.com>
2025-01-13 22:33:22 +0100

    Fix pronouns not going to desired language on profile

--- src/0DanhoLibrary/features/pronouns-page-links/transformTextIntoLinks.tsx
+++ src/0DanhoLibrary/features/pronouns-page-links/transformTextIntoLinks.tsx
@@ -7,13 +7,13 @@ export default createPatcherAfterCallback<TextModule['render']>(({ args: [props]
   if (!className || !className.includes('pronounsText')) return; // Rendered text is not pronouns text
 
   // Match "pronouns.page/@<username>"
-  const regex = text.match(/pronouns\.page\/@(\w+)/);
+  const regex = text.match(/\w{2}\.pronouns\.page\/@(\w+)/);
   if (!regex) return; // Pronouns text is not pronouns.page profile
 
-  const [matched, username] = regex;
+  const [matched] = regex;
 
   result.props.children = (
-    <a href={`https://pronouns.page/@${username}`} target="_blank" rel="noreferrer noopener">
+    <a href={`https://${matched}`} target="_blank" rel="noreferrer noopener">
       {matched}
     </a>
   ) as any;
