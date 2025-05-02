8c9abdffb2c6082797dede733895b4e5a761c259
DanielSimonsen90 <danielsimonsen90@gmail.com>
2025-05-02 15:17:58 +0200

    Fix PermissionsActions & add WakeUpNotice thumbnail

--- packages/danho-lib/src/Actions/PermissionsActions.ts
+++ packages/danho-lib/src/Actions/PermissionsActions.ts
@@ -1,11 +1,11 @@
 import { PermissionOverwrite, Snowflake } from "@discord/types";
 import Finder from "../Injections/finder";
 
-export const PermissionActions = Finder.bySourceStrings<{
+export const PermissionActions = Finder.byKeys<{
   addRecipient: (e: any, t: any, n: any, a: any) => any;
   addRecipients: (e: any, t: any, n: any, a: any, r: any) => any;
   clearPermissionOverwrite: (channelId: Snowflake, userId: Snowflake) => any;
   updatePermissionOverwrite: (channelId: Snowflake, permissionsOverwrite: PermissionOverwrite) => any;
-}>("addRecipient", "clearPermissionOverwrite", "updatePermissionOverwrite", "backupId=493683");
+}>(["addRecipient", "clearPermissionOverwrite", "updatePermissionOverwrite"]);
 
 export default PermissionActions;
